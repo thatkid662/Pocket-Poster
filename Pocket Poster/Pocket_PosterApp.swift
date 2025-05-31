4a2d58f189eb81b0b3242788a8b55654e026d5ef
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-05-31 12:43:58 -0400

    Initial Commit

---
+++ Pocket Poster/Pocket_PosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_PosterApp.swift
+//  Pocket Poster
+//
+//  Created by lemin on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_PosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
