6e0391526887359b20218ca54eec10ce6aba8357
leminlimez <59540996+leminlimez@users.noreply.github.com>
2025-06-01 05:20:50 -0400

    actually fix file picker

--- Pocket Poster/Views/ContentView.swift
+++ Pocket Poster/Views/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 import UniformTypeIdentifiers
 
+extension UIDocumentPickerViewController {
+    @objc func fix_init(forOpeningContentTypes contentTypes: [UTType], asCopy: Bool) -> UIDocumentPickerViewController {
+        return fix_init(forOpeningContentTypes: contentTypes, asCopy: true)
+    }
+}
+
 struct ContentView: View {
     // Prefs
     @AppStorage("pbHash") var pbHash: String = ""
@@ -101,6 +107,13 @@ struct ContentView: View {
         } message: {
             Text(lastError ?? "???")
         }
+    }
+    
+    init() {
+        // Fix file picker
+        let fixMethod = class_getInstanceMethod(UIDocumentPickerViewController.self, #selector(UIDocumentPickerViewController.fix_init(forOpeningContentTypes:asCopy:)))!
+        let origMethod = class_getInstanceMethod(UIDocumentPickerViewController.self, #selector(UIDocumentPickerViewController.init(forOpeningContentTypes:asCopy:)))!
+        method_exchangeImplementations(origMethod, fixMethod)
     }
 }
 
